2913202dc343c6f34ba70d63bf2ed968b4c0a5d9
princesssuna <1009suna@gmail.com>
2020-11-16 22:56:08 +0900

    todo.js

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,9 @@
 
-
 function addForm(){
     ${"#addDiv"}.css{"display", "block"};
 }
+
+document.getElementById(addDiv).style.display="none";
 
 function hideForm(){
     ${"#addDiv"}.css{"display", "none"};
