d9e71bb17a704914227493b0d35fb676a5c2cab1
princesssuna <1009suna@gmail.com>
2020-11-18 03:56:56 +0900

    todo 구현중

--- src/todo.js
+++ src/todo.js
@@ -9,13 +9,20 @@
 //    ${"#addDiv"}.css{"display", "none"};
 //}
 
-function form(){
-    var f=document.form;
-    if(f.type[0].checked==true){
-        document.all.addForm.style.display="none";
-    }else{
-        document.all.addForm.style.display="";
-    }
-}
+//function form(){
+//    var f=document.form;
+//    if(f.type[0].checked==true){
+//        document.all.addForm.style.display="none";
+//    }else{
+//        document.all.addForm.style.display="";
+//    }
+//}
+
+//$(function(){
+ //   $('.todo_add').click(function(){
+ //       $('.form').show();
+ //   });
+//});
 
 
+document.getElementById(addForm).value="추가"
